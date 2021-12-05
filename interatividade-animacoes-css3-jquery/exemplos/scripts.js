d4c25ff5a970284874aa73df146c0f56d81ebcdf
Valerio Vaz <valerio@focusmedia.pt>
2021-12-05 23:38:32 +0000

    jquery slides

---
+++ interatividade-animacoes-css3-jquery/exemplos/scripts.js
@@ -0,0 +1,32 @@
+// JavaScript normal
+
+window.onload = function() {
+    // código exemplo;
+}
+
+// jQuery option 1
+$(document).ready(function() {
+
+});
+
+// jQuery option 2
+$(function() {
+ //   alert("alerta");
+});
+
+
+// jQuery option 3
+function umAlert(){
+   // alert("hello");
+}
+
+$(document).ready(umAlert());
+
+
+// jQuery 3
+$(document).ready(function() {
+
+})
+
+
+
