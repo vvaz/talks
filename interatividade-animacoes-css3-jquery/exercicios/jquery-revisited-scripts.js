390ced9fc018366481a3b78c011d78e6c49138f3
Valerio Vaz <valerio@focusmedia.pt>
2021-12-10 19:47:18 +0000

    jquery fixes

---
+++ interatividade-animacoes-css3-jquery/exercicios/jquery-revisited-scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    
+    // pergunta 1
+    $('.box').css('color', '#fff');
+
+    $('.box').html('Forma de declarar uma <strong>Strong</strong>');
+    
+    $('.box').text('Forma de declarar uma <div>');
+
+    $('.box').addClass('novaClass');
+
+
+
+
+
+
+    // editar as ULs
+/*
+    $('li.azul').css('background', 'blue');
+    $('li.verde').css('background', 'green');
+    $('li.vermelho').css('background', 'red');
+
+
+    // botão
+
+    $('button').css({'background': 'green', 'color': 'white'});
+
+    // IDs
+
+    $('.lione').css({'background':'pink', 'font-size': '30px'});
+    $('#litwo').css({'color': '#000', 'font-size': '30px', 'background': 'green'});
+    $('#lithree').css({'color': 'blue', 'font-size': '20px', 'background':'red', 'padding': '2rem'});
+*/
+
+});
